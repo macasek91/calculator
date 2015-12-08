fd6420206c5ed2e586ae9f54558acb2e99dd2c73
macasek91 <okaymax99@gmail.com>
2015-12-08 14:09:14 -0500

    first commit

---
+++ Calculator/src/Gui.java
@@ -0,0 +1,15 @@
+
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Gui extends JFrame{
+	private JTextField answerfield;
+	private JButton one, two, three, four, five, six, 
+	seven, eight, nine, zero, add, sub, multiply, div,
+	equals;
+	private String stemp1, stemp2, sanswer;
+	
+}
